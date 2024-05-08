4728ad8f8328fd30b977bf9485bace356eefa3a7
uni-SanjayS <sanjay.s@uniphore.com>
2024-05-08 16:30:10 +0530

    My new commit

--- 9.Puppy_package_import/main.go
+++ 9.Puppy_package_import/main.go
@@ -1,6 +1,19 @@
 package main
 
-import "github.com/uni-SanjayS/puppy"
-func main() {
+import (
+	"fmt"
 
+	"github.com/uni-SanjayS/puppy"
+)
+
+func main() {
+	s1 := puppy.Bark()
+	s2 := puppy.Barks()
+
+	fmt.Println(s1)
+	fmt.Println(s2)
+
+	// Also we can able to print like this
+	fmt.Println(puppy.Bark)
+	fmt.Println(puppy.Barks)
 }
