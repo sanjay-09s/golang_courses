1d97f342b8161ca8f1602c0358e1ea73244afd0a
uni-SanjayS <sanjay.s@uniphore.com>
2024-05-10 12:22:42 +0530

    New files added

--- 11.versions-example/main.go
+++ 11.versions-example/main.go
@@ -5,3 +5,13 @@ import "fmt"
 func main() {
 	fmt.Println("Hello, we're working on vaersion tags")
 }
+
+// "git tag" shows nothing when there is no tag
+// To set up the tag "git tag v1.0.0" here we can use any version
+// to see info about the tag "git show v1.0.0"
+
+// also we can get the latest module dependices using the commit ID
+// eg: "go get github.com/uni-SanjayS/puppy@commit_ID"
+
+// To get the latest commit "go get github.com/uni-SanjayS/puppy@latest"
+// to get the particular version of that code "go get github.com/uni-SanjayS/puppy@v1.0.0"
