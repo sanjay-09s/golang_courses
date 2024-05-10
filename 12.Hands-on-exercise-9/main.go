f2b6bf52eeae44529e302b44566f49061632215c
uni-SanjayS <sanjay.s@uniphore.com>
2024-05-10 12:34:40 +0530

    Msg from terminal

---
+++ 12.Hands-on-exercise-9/main.go
@@ -0,0 +1,11 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("Hello Gophers. This is from terminal")
+}
+
+// For creating the new file "touch main.go"
+// for writing those files "nano main.go"
+// add the code in the mini complier
